8aeadf16fa55b383df73f8fd8ff44463681d5204
RyuichiSaito <90978149+RyuichiSaito@users.noreply.github.com>
2023-12-23 15:45:36 +0900

    スコア計算・経過時間表示

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ScoreManager : MonoBehaviour
+{
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UpdateScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        UpdateScoreText();
+    }
+
+    void UpdateScoreText()
+    {
+        scoreText.text = $"{score}円";
+    }
+
+    public void AddScore(int amount)
+    {
+        score += amount;
+    }
+}
