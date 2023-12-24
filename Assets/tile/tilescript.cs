0bfbe60b5a483fbdbeb07044f97e0f313d20ce2e
ko_cha78 <kota1725@gmail.com>
2023-12-24 23:48:08 +0900

    マスタップ時のSE

--- Assets/tile/tilescript.cs
+++ Assets/tile/tilescript.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class tilescript : MonoBehaviour
 {
+    public AudioClip sound1;
+    AudioSource seAudioSource;
     [SerializeField] private Transform emptySpace = null;
     private Camera _camera;
     // Start is called before the first frame update
     void Start()
     {
+        seAudioSource = GetComponent<AudioSource>();
         _camera = Camera.main;
     }
 
@@ -23,6 +27,7 @@ public class tilescript : MonoBehaviour
             {
                 if (Vector2.Distance(emptySpace.position, hit.transform.position) < 3)
                 {
+                    seAudioSource.PlayOneShot(sound1);
                     Vector2 lastEmptySpacePosition = emptySpace.position;
                     emptySpace.position = hit.transform.position;
                     hit.transform.position = lastEmptySpacePosition;
