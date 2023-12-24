de5b85d86449dec5e5c79f02e3a8a780273ccad7
Yo Kobayashi <82624334+KobayashiYoh@users.noreply.github.com>
2023-12-24 23:43:40 +0900

    よいこのアニメーションを追加

--- Assets/Scripts/GoodChildController.cs
+++ Assets/Scripts/GoodChildController.cs
@@ -2,18 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// よいこオブジェクトを動かすためのController
-/// </summary>
 public class GoodChildController : MonoBehaviour
 {
-    void Start()
-    {
+    // アニメーションのパラメータ
+    public float minHeight = 1.0f;
+    public float maxHeight = 1.2f;
+    public float animationSpeed = 0.1f;
 
-    }
+    // アニメーションの方向を管理するフラグ
+    private bool isGrowing = true;
 
     void Update()
     {
+        // アニメーションの実行
+        Animate();
+    }
 
+    void Animate()
+    {
+        // スケールを変更する
+        float newHeight = transform.localScale.y + (isGrowing ? 1 : -1) * animationSpeed * Time.deltaTime;
+
+        // スケールが一定範囲外になった場合、方向を切り替える
+        if (newHeight < minHeight || newHeight > maxHeight)
+        {
+            isGrowing = !isGrowing;
+        }
+
+        // 新しいスケールを設定
+        transform.localScale = new Vector3(transform.localScale.x, Mathf.Clamp(newHeight, minHeight, maxHeight), transform.localScale.z);
     }
 }
